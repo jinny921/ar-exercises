01bd3d242af14c0ab8e96dc7233ef412a407efc3
Jinny Huang <jinny921@gmail.com>
2017-05-09 23:40:44 +0000

    completed exercise 5, created employees and relations between classes

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,37 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+# @store4 = Store.find(4)
+# @store5 = Store.find(5)
+# @store6 = Store.find(5)
+
+@store1.employees.create(
+  first_name: "Rachel", 
+  last_name: "Green", 
+  hourly_rate: 40)
+@store1.employees.create(
+  first_name: "Monica", 
+  last_name: "Geller", 
+  hourly_rate: 45)
+
+@store1.employees.create(
+  first_name: "Ross", 
+  last_name: "Geller", 
+  hourly_rate: 60)
+
+@store2.employees.create(
+  first_name: "Chandler", 
+  last_name: "Bing", 
+  hourly_rate: 80)
+
+@store2.employees.create(
+  first_name: "Joey", 
+  last_name: "Tribioni", 
+  hourly_rate: 25)
+
+@store2.employees.create(
+  first_name: "Pheobe", 
+  last_name: "Buffay", 
+  hourly_rate: 100)
