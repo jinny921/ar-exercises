8f87fb8a6ca736691222c7101818b0ba632cf2dc
Jinny Huang <jinny921@gmail.com>
2017-05-09 23:25:43 +0000

    completed exercise 5, calculated sum of revenue, avg of revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@company_revenue = Store.all.sum("annual_revenue")
+puts "Total revenue of the company: $#{@company_revenue}"
+
+@average_revenue = @company_revenue / Store.count
+puts "Average revenue of all the stores: $#{@average_revenue}"
+
+@outstanding_store_count = Store.select { |x| x[:annual_revenue] >= 1000000 }.count
+puts "We have #{@outstanding_store_count} of stores that are generating more equal or more than 1M annual revenue."
+
+
